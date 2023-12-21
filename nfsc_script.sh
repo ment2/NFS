09a6e69ccf9a00c1267bbd88bd8cd58fa2bbfdde
root <root@Ubuntu.myguest.virtualbox.org>
2023-12-21 12:27:24 +0300

    «proto=UDP»

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -4,7 +4,7 @@
 yum install -y nfs-utils
 
 # Добавление записи в /etc/fstab для монтирования NFS
-echo "192.168.57.1:/srv/share/ /mnt nfs vers=3,proto=tcp,auto,x-systemd.automount 0 0" >> /etc/fstab
+echo "192.168.57.1:/srv/share/ /mnt nfs vers=3,proto=udp,auto,x-systemd.automount 0 0" >> /etc/fstab
 
 # Перезагрузка системдемона
 systemctl daemon-reload
